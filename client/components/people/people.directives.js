4f7016dde8e6d2eb8040a20259f4d413b8b09a8c
Simon Morley <simon@polkaspots.com>
2018-02-23 18:41:59 +0000

    twitters

--- client/components/people/people.directives.js
+++ client/components/people/people.directives.js
@@ -342,16 +342,16 @@ app.directive('listPeople', ['People', 'Location', 'Audience', '$timeout', '$loc
     var buildLocation = function() {
       scope.location = {
         slug: $routeParams.id,
-        setup: {
-          splash: attrs.splashSetup,
-          integrations: JSON.parse(attrs.integrationsSetup)
-        },
-        paid: JSON.parse(attrs.locationPaid)
+        // setup: {
+        //   splash: attrs.splashSetup,
+        //   integrations: JSON.parse(attrs.integrationsSetup)
+        // },
+        // paid: JSON.parse(attrs.locationPaid)
       };
     };
 
     var checkForGuide = function() {
-      // buildLocation();
+      buildLocation();
       // if ($location.path().split('/')[2] !== 'people' && (scope.location.setup.splash === false || scope.location.setup.integrations === false || scope.location.paid === false)) {
       //   $location.path('/' + scope.location.slug + '/guide');
       // } else {

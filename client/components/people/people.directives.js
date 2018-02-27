7ce7e40417c512dfbda35ba87b276369a6073d5f
Ewan <ewan.s@cucumberwifi.io>
2018-02-27 11:58:01 +0000

    people nav bar / guide fix

--- client/components/people/people.directives.js
+++ client/components/people/people.directives.js
@@ -7,7 +7,7 @@ app.directive('listPeople', ['People', 'Location', 'Audience', '$timeout', '$loc
   var link = function(scope, el, attrs, controller) {
 
     scope.currentNavItem = 'people';
-    // scope.location = {slug: $routeParams.id};
+    scope.location = {slug: $routeParams.id};
     scope.predicates_changed = $routeParams.predicates_changed;
 
     var defaultBlob = [{
@@ -365,24 +365,25 @@ app.directive('listPeople', ['People', 'Location', 'Audience', '$timeout', '$loc
     var buildLocation = function() {
       scope.location = {
         slug: $routeParams.id,
-        // setup: {
-        //   splash: attrs.splashSetup,
-        //   integrations: JSON.parse(attrs.integrationsSetup)
-        // },
-        // paid: JSON.parse(attrs.locationPaid)
+        setup: {
+          splash: attrs.splashSetup,
+          integrations: attrs.integrationsSetup
+        },
+        paid: attrs.locationPaid
       };
     };
 
     var checkForGuide = function() {
       buildLocation();
-      // if ($location.path().split('/')[2] !== 'people' && (scope.location.setup.splash === false || scope.location.setup.integrations === false || scope.location.paid === false)) {
-      //   $location.path('/' + scope.location.slug + '/guide');
-      // } else {
+      var falses = [false, 'false'];
+      if ($location.path().split('/')[2] !== 'people' && (falses.includes(scope.location.setup.splash) || falses.includes(scope.location.setup.integrations) || falses.includes(scope.location.paid))) {
+        $location.path('/' + scope.location.slug + '/guide');
+      } else {
       setParams();
       getAudiences().then(function() {
         getPeople();
       });
-      // }
+      }
     };
 
     var init = function() {

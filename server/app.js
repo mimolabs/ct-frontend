7786aeb73f4c4000a26e8e310f70d1267693b94d
Ewan <ewan.s@cucumberwifi.io>
2018-04-04 17:48:51 +0100

    twitter auth app.js

--- server/app.js
+++ server/app.js
@@ -9,10 +9,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var helmet = require('helmet');
+var oauth = require('oauth');
 
 var config = require('./config/environment');
 // Setup server
 var app = express();
+const callback_url = "http://app.mimo.test:9090/auth/twitter/callback";
+const TWITTER_CONSUMER_KEY = 'wBAOinMVjBtnxizBgQbBy1YFp';
+const TWITTER_CONSUMER_SECRET = 'CORqWenWflNzb7WBoT12Tq5oV6ovHxuV1IrOKXam2wLRMUzXl5'
 
 app.use(helmet());
 app.use(helmet.xframe('deny'));
@@ -22,6 +26,42 @@ app.use(helmet.hidePoweredBy());
 
 var server = require('http').createServer(app);
 require('./config/express')(app);
+
+var consumer = new oauth.OAuth(
+  "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token",
+  (TWITTER_CONSUMER_KEY || '123'), (TWITTER_CONSUMER_SECRET || '123'), "1.0A", callback_url, "HMAC-SHA1");
+
+app.get('/auth/twitter', function(req, res) {
+  req.session.location = req.query.location;
+  req.session.sender_name = req.query.sender_name;
+  consumer.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+    if (error) {
+      res.send("Error getting OAuth request token. Please contact support or try again.");
+    } else {
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+oauthToken);
+    }
+  });
+});
+
+app.get('/auth/twitter/callback', function(req, res) {
+  consumer.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+    if (error) {
+      res.send("Error getting OAuth access token. Please contact support or try again.");
+    } else {
+      req.session.oauthAccessToken = oauthAccessToken;
+      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+      req.session.screen_name = results.screen_name;
+      if (!req.session.location || !req.session.oauthAccessToken || !req.session.oauthAccessTokenSecret) {
+        res.redirect('/');
+        return;
+      }
+      res.redirect('/#/' + req.session.location + '/campaigns/senders/new?sender_name=' + req.session.sender_name + '&oauth_access_token=' + req.session.oauthAccessToken + '&oauth_access_secret=' + req.session.oauthAccessTokenSecret + '&twitter_name=' + req.session.screen_name);
+    }
+  });
+});
+
 require('./routes')(app);
 
 // Start server
